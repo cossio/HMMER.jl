98e5d9af0c53a3737ead8c76e44160692fb73dd5
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-08-03 20:43:24 +0200

    clean up

--- scripts/install-dev-tools.sh
+++
@@ -1,7 +0,0 @@
-# Install ohmyzsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# chsh -s $(which zsh)
-
-# Install Julia through juliaup
-curl -fsSL https://install.julialang.org | sh -s -- --yes
