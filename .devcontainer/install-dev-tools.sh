7c6c9e92d7c51bb3cbd16c3fe21d17f89b2d7193
cossio <cossio@users.noreply.github.com>
2023-08-08 21:41:27 +0000

    dev

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,1 +1,4 @@
-curl -fsSL https://install.julialang.org | sh -s -- --yes
+# rye (https://rye-up.com)
+curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
+echo 'source "$HOME/.rye/env"' >> ~/.bashrc
+source "$HOME/.rye/env"
